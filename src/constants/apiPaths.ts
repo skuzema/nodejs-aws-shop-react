3a1065dc02cff38e91fdfe36536d6344f6858801
Sergejs Kuzema <s.kuzema@gmail.com>
2024-06-20 23:42:19 +0300

    feat: backend

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://0iyu3mo6ch.execute-api.eu-north-1.amazonaws.com/prod",
+  product: "https://1ciaofol7e.execute-api.eu-north-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
