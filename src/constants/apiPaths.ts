f5d40b07bffdf2955bf015919bc5b8db973d61f8
Sergejs Kuzema <s.kuzema@gmail.com>
2024-08-03 14:46:47 +0300

    feat: task 9

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://ecy2nn2cgk.execute-api.eu-north-1.amazonaws.com/prod",
   import: "https://fie4vkmwoi.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://ecy2nn2cgk.execute-api.eu-north-1.amazonaws.com/prod",
-  cart: "http://skuzema-cart-api-env.eu-north-1.elasticbeanstalk.com/api",
+  cart: "https://vtxrcdhyeh.execute-api.eu-north-1.amazonaws.com/api",
 };
 
 export default API_PATHS;
