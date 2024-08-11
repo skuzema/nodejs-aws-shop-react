03640da1d8762a2ffbcd6decff932edf4bae27b9
Sergejs Kuzema <s.kuzema@gmail.com>
2024-08-11 18:33:48 +0300

    feat: build

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://ecy2nn2cgk.execute-api.eu-north-1.amazonaws.com/prod",
   order: "https://ecy2nn2cgk.execute-api.eu-north-1.amazonaws.com/prod",
   import: "https://fie4vkmwoi.execute-api.eu-north-1.amazonaws.com/prod",
-  bff: "https://ecy2nn2cgk.execute-api.eu-north-1.amazonaws.com/prod",
+  bff: "https://xflalntnt9.execute-api.eu-north-1.amazonaws.com",
   cart: "https://vtxrcdhyeh.execute-api.eu-north-1.amazonaws.com/api",
 };
 
